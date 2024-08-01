59f2b253ad839914ca065241b662cdbac487f923
Agustina Luduena <agusluduena4@gmail.com>
2024-08-01 17:06:45 -0300

    Login view

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -24,6 +24,7 @@ form.addEventListener("submit", (e) => {
   return response.json(); // Obtener el cuerpo de la respuesta en formato JSON
 })
 .then(data => {
+  console.log(data)
   localStorage.setItem("proyecto_backend", data.token); // Guardar el token en localStorage
   window.location.replace("/products");
 })
@@ -31,3 +32,65 @@ form.addEventListener("submit", (e) => {
   console.error(error);
 })
 })
+
+
+// // Login de formulario
+// document.addEventListener('DOMContentLoaded', function () {
+//   const loginForm = document.getElementById('loginForm');
+
+//   if (loginForm) {
+//       loginForm.addEventListener('submit', function (event) {
+//           event.preventDefault();
+
+//           const formData = new FormData(loginForm);
+//           const obj = {};
+//           formData.forEach((val, key) => obj[key]=val);
+//           const errorMessage = document.getElementById('errorMessage');
+
+//           fetch('https://backend-final-production-8834.up.railway.app/api/sessions/login', {
+//               method: 'POST',
+//               body: JSON.stringify(obj),
+//               headers: {
+//                   'Content-Type': 'application/json',
+//                   "Access-Control-Allow-Origin": "*"
+//               }
+//           })
+//           .then(response => {
+//               if (response.status === 200) {
+//                   // La respuesta exitosa
+//                   return response.json();
+//               } else {
+//                   // Si la respuesta no es exitosa, mostrar un mensaje de error
+//                   errorMessage.textContent = 'Email o contraseña incorrectos. Por favor, inténtalo de nuevo.';
+//                   errorMessage.style.display = 'block';
+//                   throw new Error('Credenciales incorrectas');
+//               }
+//           })
+//           .then(data => {
+//               // Extraer el token de la respuesta JSON
+//               // Almacenar el token en el almacenamiento local
+//               const token = data.access_token;
+//               const userId = data.userId;
+//               const userRole = data.userRole;
+
+//               localStorage.setItem('token', token);
+//               localStorage.setItem('userId', userId);
+//               localStorage.setItem('userRole', userRole);
+//               console.log("Token:", token);
+//               console.log("userId:", userId);
+//               console.log("user rol:", userRole);
+//               console.log("Inicio de sesión exitoso!");
+
+//               if (userRole === 'admin') {
+//                   window.location.href = `https://backend-final-production-8834.up.railway.app/api/sessions/dashboard/${userId}`;
+//               }
+//               else {
+//                   window.location.href = "https://backend-final-production-8834.up.railway.app/api/products";
+//               }
+//           })
+//           .catch(error => {
+//               console.error('Error en el inicio de sesión:', error);
+//           });
+//       });
+//   }
+// });
