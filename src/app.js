4527df2b54e8585c749eaec647bccc3f7651fa7a
Agustina Luduena <agusluduena4@gmail.com>
2024-08-03 20:43:35 -0300

    Cleaning

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import connectDb from './config/database.js';
 import router from './routes/apiRoutes.js';
 import middlewares from './config/middlewaresConfig.js';
 import createExpressApp from './config/createApp.js';
-import socketServer from './config/createSocket.js';
 import { errorHandler } from './middlewares/errorHandler.middleware.js';
 
 async function main(){
@@ -20,9 +19,6 @@ async function main(){
 
   //ErrorHandler
   app.use(errorHandler) 
-
-  //SocketIO
-  socketServer()
 
 }
 
